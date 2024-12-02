19268cb5a2cc1490d99a5c4b665a9b4ce51f74cd
Chaz Coats <chazcoats2@hotmail.com>
2024-12-02 15:07:47 -0500

    refactoring server code to fix issues and implement error catching

--- server/server.js
+++ server/server.js
@@ -2,29 +2,40 @@ require('dotenv').config()
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
-
 const {
     ApolloServerPluginDrainHttpServer, 
     ApolloServerPluginLandingPageLocalDefault} = require('apollo-server-core');
-const { http } = require('http');
+const http = require('http');
+const cors = require('cors');
+const PORT = process.env.PORT || 3001;
 
-async function startServer(typeDefs, resolvers) {
-    const app = express();
-    const httpServer = http.createServer(app);
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        csrfPrevention: true,
-        cache: 'bounded',
-        plugins: [
-            ApolloServerPluginDrainHttpServer({ httpServer }),
-            ApolloServerPluginLandingPageLocalDefault({ embedded: true})
-        ]
-    });
-    await server.start();
-    server.applyMiddleware({ app });
-    await new Promise(resolve => httpServer.listen({ port: 3001 }, resolve));
-    console.log(`��� Server ready at http://localhost:3001${server.graphqlPath}`);
+async function startServer() {
+    try {
+        const app = express();
+
+        app.use(cors());
+        app.use(express.json());
+
+        const httpServer = http.createServer(app);
+        const server = new ApolloServer({
+            typeDefs,
+            resolvers,
+            csrfPrevention: true,
+            cache: 'bounded',
+            plugins: [
+                ApolloServerPluginDrainHttpServer({ httpServer }),
+                ApolloServerPluginLandingPageLocalDefault({ embedded: true})
+            ]
+        });
+
+        await server.start();
+        server.applyMiddleware({ app });
+
+        await new Promise(resolve => httpServer.listen({ port: PORT }, resolve));
+        console.log(`��� Server ready at http://localhost${PORT}${server.graphqlPath}`);
+    } catch (error) {
+        console.error('Error starting server:', error.message);
+    }
 }
 
-startServer(typeDefs, resolvers);
+startServer();
